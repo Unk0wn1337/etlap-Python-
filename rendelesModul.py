f2779a77b5c7f61e5d8efeeb1d4300c296a90c4c
Unk0wn1337 <kelogames@gmail.com>
2023-11-30 11:29:10 +0100

    javitott kod duplikalas megszunt

--- rendelesModul.py
+++ rendelesModul.py
@@ -12,14 +12,16 @@ def rendelesFelvetel(lista):
     while index < len(lista):
         bekeresKaja = int(input("Kérem a kajának a sorszámát"))
         if bekeresKaja == 1:
-            rendeltLista.append(lista[index]-1)
+            rendeltLista.append(str(lista[index-1]))
             veglegesFizetesAr += 3000
         if bekeresKaja == 2:
-            rendeltLista.append(lista[index]-1)
+            rendeltLista.append(str(lista[index-1]))
             veglegesFizetesAr += 2500
         if bekeresKaja == 3:
-            rendeltLista.append(lista[index]-1)
+            rendeltLista.append(str(lista[index-1]))
             veglegesFizetesAr += 4000
+        index+=1
+    print(f"Végleges ár {veglegesFizetesAr}")
 
 
 
